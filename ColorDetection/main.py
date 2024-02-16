517494859e67b87b20bf25b75b0bb4d448fde34c
SamManoli975 <C23330671@mytudublin.ie>
2024-02-16 10:49:00 +0000

    making util.py and creating a function to get the range of values of the colour the camera should track and masking it in the live camera

--- ColorDetection/main.py
+++ ColorDetection/main.py
@@ -1,23 +1,24 @@
 import cv2 as cv
 import os
+from util import get_limits
 # import numpy as np
 
-img = cv.imread('ColorDetection/images/venicecolor.jpg')
-# img2 =cv.imread('./images/venicecolor.jpg')
-# Uncomment the line below if you want to resize the image
+yellow = [0,255,255] 
+cap = cv.VideoCapture(0)
+cv.namedWindow('ColorDetection/frame', cv.WINDOW_NORMAL)
 
-# img = cv.resize(img2, (200, 200))
-# cv.imshow('image', img2)
-new_size = (450, 340)
-img = cv.resize(img, new_size)
+while True:
+    ret, frame = cap.read()
 
-cv.imshow('image', img)
+    hsvImage = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-# def rescaleFrame(frame, scale=0.5):
-#     width = int(frame.shape[1] * scale)
-#     height = int(frame.shape[0] * scale)
-#     dimensions = (width, height)
-# resize_img = cv.resize(img,(0, 0),fx=0.5, fy=0.7, interpolation = cv.INTER_AREA)
+    lower, upper = get_limits(color = yellow)
 
-cv.waitKey(0)
+    mask = cv.inRange(hsvImage, lower,upper)
+
+    cv.imshow('ColorDetection/frame', mask)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv.destroyAllWindows()
