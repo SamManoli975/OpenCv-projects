11b531a43f612870fcde476ca1aaa11f96cd0c80
SamManoli975 <C23330671@mytudublin.ie>
2024-02-16 11:00:02 +0000

    detecting yellow around the screen and creating a bounding box around it

--- ColorDetection/main.py
+++ ColorDetection/main.py
@@ -19,9 +19,12 @@ while True:
 
     mask_ = Image.fromarray(mask)
     bounding_box = mask_.getbbox()
+    if bounding_box is not None:
+        x, y, w, h = bounding_box
+        frame = cv.rectangle(frame, (x, y), (w, h), (0, 255, 0), 5)
 
-    print(bounding_box)
-    cv.imshow('ColorDetection/frame', mask)
+    # print(bounding_box)
+    cv.imshow('ColorDetection/frame', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
