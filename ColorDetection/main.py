d39a6c5bfab1f9226d9b368147faba322deb691d
SamManoli975 <C23330671@mytudublin.ie>
2024-02-16 09:47:07 +0000

    added some images and corrected the image paths, also begining the basics of imread resize and imshow

--- ColorDetection/main.py
+++ ColorDetection/main.py
@@ -1,10 +1,23 @@
 import cv2 as cv
-import numpy as np
+import os
+# import numpy as np
 
-img = cv.imread('img2.jpg')
+img = cv.imread('ColorDetection/images/venicecolor.jpg')
+# img2 =cv.imread('./images/venicecolor.jpg')
 # Uncomment the line below if you want to resize the image
-# img = cv.resize(img, (500, 500))
+
+# img = cv.resize(img2, (200, 200))
+# cv.imshow('image', img2)
+new_size = (450, 340)
+img = cv.resize(img, new_size)
+
 cv.imshow('image', img)
+
+# def rescaleFrame(frame, scale=0.5):
+#     width = int(frame.shape[1] * scale)
+#     height = int(frame.shape[0] * scale)
+#     dimensions = (width, height)
+# resize_img = cv.resize(img,(0, 0),fx=0.5, fy=0.7, interpolation = cv.INTER_AREA)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
