2bbacb65163de883b5a84cf69991528e2189aa71
SamManoli975 <C23330671@mytudublin.ie>
2024-02-17 16:14:36 +0000

    adding a bounding box around the text i am detecting and also displaying the text using cv.putText

--- TextRecognition/main.py
+++ TextRecognition/main.py
@@ -7,11 +7,24 @@ img = cv.imread('TextRecognition/media/roadsign1.jpg')
 img = cv.resize(img, (640, 480))
 # cv.imshow('image',img)
 
-
-# reader = easyocr.Reader(['en'], gpu = False)
-# text= reader.readtext(img)
+# text = pytesseract.image_to_string(img)
 # print(text)
-text = pytesseract.image_to_string(img)
-print(text)
+detections = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
+
+# Iterate over the detected words and draw bounding boxes
+for i in range(len(detections['text'])):
+    x, y, w, h = detections['left'][i], detections['top'][i], detections['width'][i], detections['height'][i]
+    conf = int(detections['conf'][i])
+    
+    # Only draw bounding box if the confidence is high enough (adjust threshold as needed)
+    if conf > 60:
+        cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        text = detections['text'][i]
+        cv.putText(img, text, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv.LINE_AA)
+
+# Display the image with bounding boxes around words
+cv.imshow('image_with_boxes', img)
+
 cv.waitKey(0)
 
--- TextRecognition/tempCodeRunnerFile.py
+++ TextRecognition/tempCodeRunnerFile.py
@@ -1,3 +1,11 @@
-# r.Reader(['en'], gpu = False)
-# text= reader.readtext(img)
-# print(text)
+boxes = pytesseract.image_to_boxes(img)
+
+for box in boxes.splitlines():
+    box = box.split()
+    x, y, w, h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+    
+    # Draw the bounding box on the image
+    cv.rectangle(img, (x, img.shape[0] - y), (w, img.shape[0] - h), (0, 255, 0), 2)
+
+# Display the image with bounding boxes
+cv.imshow('image_with_boxes', img)
