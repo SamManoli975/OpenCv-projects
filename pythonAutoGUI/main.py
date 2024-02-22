0e41c45c16053595db00bcba5590b73875a63072
SamManoli975 <C23330671@mytudublin.ie>
2024-02-22 10:16:24 +0000

    reseting commits

---
+++ pythonAutoGUI/main.py
@@ -0,0 +1,14 @@
+import pyautogui
+# pyautogui.click()
+pyautogui.moveRel(200, 200, duration=6) 
+# pyautogui.typewrite(['h', 'e', 'l', 'l', 'o'])
+# pyautogui.keyDown('alt')
+# pyautogui.press('tab')
+# pyautogui.press('tab')
+# pyautogui.press('tab')
+# pyautogui.keyUp('alt')
+# pyautogui.moveTao(50, 20)
+# pyautogui.click()
+print(pyautogui.position())
+
+
